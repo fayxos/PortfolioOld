0128db0b03477b1e15622659c589444396702f40
qxfayxos <felixhaag@hotmail.de>
2022-02-15 21:45:44 +0100

    loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -52,20 +52,7 @@ const Loader = ({ finishLoading }) => {
         easing: 'easeInOutQuart',
         strokeDashoffset: [anime.setDashoffset, 0],
       })
-      .add({
-        targets: '#logo #B',
-        duration: 700,
-        easing: 'easeInOutQuart',
-        opacity: 1,
-      })
-      .add({
-        targets: '#logo',
-        delay: 500,
-        duration: 300,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        scale: 0.1,
-      })
+      
       .add({
         targets: '.loader',
         duration: 200,
