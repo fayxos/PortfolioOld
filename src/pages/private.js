a7e7e8c0145cbb5cbded004f46ef03bef9757a3e
qxfayxos <felixhaag@hotmail.de>
2022-11-23 18:24:38 +0100

    login fix 7

--- src/pages/private.js
+++ src/pages/private.js
@@ -1,20 +1,22 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/layout"
-import Profile from "../components/profile"
-import Upload from "../components/upload"
-import Login from "../components/login"
-import PrivateRoute from "../components/privateRoute"
+import Profile from "../components/private/profile"
+import Upload from "../components/private/upload"
+import Login from "../components/private/login"
+import PrivateRoute from "../components/private/privateRoute"
 
 const Private = () => (
   <Layout>
     <Router>
-      <PrivateRoute path="/private/profile" component={Profile} />
-      <PrivateRoute path="/private/upload" component={Upload} />
+      
       <Login path="/private/login" />
     </Router>
   </Layout>
 )
+
+//<PrivateRoute path="/private/profile" component={Profile} />
+//<PrivateRoute path="/private/upload" component={Upload} />
 
 export default Private
 
